a71893c90dd686ec2b6a1e2873fa2a8a89e92cfd
JackHaoyingZhou <zhouhaoying96@outlook.com>
2024-03-11 04:27:03 -0400

    update

--- 3d_med_phantom/V1.3/run_env.sh
+++ 3d_med_phantom/V1.3/run_env.sh
@@ -1,2 +1,1 @@
-
-ambf_simulator --launch_file launch.yaml -l 1,2,3 -p120 -t1 --override_max_comm_freq 120
+/home/jackzhy/ambf/bin/lin-x86_64/ambf_simulator --launch_file launch.yaml -l 1,2,3 -p200 -t1 --override_max_comm_freq 100

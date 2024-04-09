3c322c573c69caa5ba022260133876550cae5691
JackHaoyingZhou <hzhou62@lcsr-dvrk-09.win.ad.jhu.edu>
2024-04-09 15:08:59 -0400

    teleop success test

--- 3d_med_phantom/V1.4/run_env.sh
+++ 3d_med_phantom/V1.4/run_env.sh
@@ -1,1 +1,1 @@
-/home/jackzhy/ambf/bin/lin-x86_64/ambf_simulator --launch_file launch.yaml -l 0,1,2,4 -p120 -t1 --override_max_comm_freq 120
+/home/hzhou62/ambf/bin/lin-x86_64/ambf_simulator --launch_file launch.yaml -l 0,1,2,3,4,5 -p120 -t1 --override_max_comm_freq 120
